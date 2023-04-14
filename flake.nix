831c087d129e11a24683f5fe13a8878bd53c85f7
Andy Scott <andy.g.scott@gmail.com>
2023-04-14 09:57:24 -0700

    +pre-commit

--- flake.nix
+++ flake.nix
@@ -21,6 +21,44 @@
           inherit system;
           overlays = [ inputs.gomod2nix.overlays.default ];
         };
+
+        # This function produces a pre-commit installation with fixed tooling/entries
+        # on the path. This is tricky because pre-commit is already wrapped and patched
+        # by the upstream nix derivation. We don't really want to override that
+        # derivation because we want to leverage cached results.
+        pre-commit-with-path =
+          let upstream = pkgs.pre-commit; in
+          path: pkgs.stdenv.mkDerivation rec {
+            name = "pre-commit";
+            inherit (upstream) buildInputs propagatedBuildInputs;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            phases = [ "installPhase" ];
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r ${upstream}/* $out/
+              for f in $out/lib/*/site-packages/pre_commit/resources/hook-tmpl; do
+                # Here we don't substitute in our full path. If we did then we can't
+                # update the paths available to an installed hook without requiring
+                # users to re-install the hook itself. This isn't a very desireable
+                # behavior!
+                substituteInPlace $f \
+                  --replace ${upstream}/bin/pre-commit pre-commit
+              done
+              GLOBIGNORE=".:.."
+              for f in $out/bin/*; do
+                # Here we substitute in our full path because we're patching the
+                # wrappers produced by the upstream derivation. We need all
+                # references to stay within this new derivation.
+                substituteInPlace $f \
+                  --replace ${upstream} $out
+              done
+              chmod +w $out/bin/
+              chmod +w $out/bin/pre-commit
+              wrapProgram $out/bin/pre-commit \
+                --set PATH ${pkgs.lib.makeBinPath path}
+            '';
+          };
       in
       {
         legacyPackages.default = pkgs;
@@ -29,12 +67,16 @@
             packages = with pkgs; [
               just
               go_1_20
-              gomod2nix
               gopls
               gotools
-              golangci-lint
-              nixpkgs-fmt
-              statix
+              (pre-commit-with-path [
+                git
+                golangci-lint
+                gomod2nix
+                nixpkgs-fmt
+                statix
+                yamlfmt
+              ])
             ];
           };
         };
